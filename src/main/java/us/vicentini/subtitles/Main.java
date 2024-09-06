01ecc59741041926e341ae7c09c67f1abe4cf1bb
Henrique Vicentini <henrique@vicentini.us>
2024-09-05 21:19:24 -0700

    Small change, pushing some code to revive the project

--- src/main/java/us/vicentini/subtitles/Main.java
+++ src/main/java/us/vicentini/subtitles/Main.java
@@ -16,12 +16,12 @@ import us.vicentini.subtitles.util.SubtitlesUtil;
 
 
 /**
- *
  * @author Shulander
  */
 public class Main {
 
     private final static Log log = LogFactory.getLog(us.vicentini.subtitles.Main.class);
+    public static final int ITERATIONS = 8;
 
     static {
         if ((new File("config/log4j.xml")).exists()) {
@@ -36,59 +36,61 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
         log.info("started");
-        String inputEngPathFile = "input\\legenda.eng.srt";
-        String inputPorPathFile = "input\\legenda.pob.srt";
-//        String inputPorPathFile = "output\\legenda.eng.srt";
-        SubRip engSubFormat = new SubRip();
-        Subtitle engSub = engSubFormat.parse(FileUtils.fileRead(inputEngPathFile));
+        String inputSyncPathFile = "input/The.Beekeeper.2024.iTA-ENG.PROPER.Bluray.1080p.x264-CYBER.ita.srt";
+        String inputUnsyncPathFile = "input/The.Beekeeper.2024.iTA-ENG.PROPER.Bluray.1080p.x264-CYBER.eng.srt";
+//        String inputUnsyncPathFile = "output\\legenda.eng.srt";
+        SubRip syncSubFormat = new SubRip();
+        Subtitle synchronizedSubtitle = syncSubFormat.parse(FileUtils.fileRead(inputSyncPathFile));
 
-        SubRip porSubFormat = new SubRip();
-        Subtitle porSub = porSubFormat.parse(FileUtils.fileRead(inputPorPathFile));
 
-        double original = SubtitlesUtil.compareSubtitles(engSub, porSub);
+        SubRip unsyncSubFormat = new SubRip();
+        Subtitle unsynchronizedSubtitle = unsyncSubFormat.parse(FileUtils.fileRead(inputUnsyncPathFile));
 
-        BasicSyncSubtitles syncTool = new BasicSyncSubtitles(engSub, porSub);
+        double original = SubtitlesUtil.compareSubtitles(synchronizedSubtitle, unsynchronizedSubtitle);
 
-        double compareValue = 0.0;
-        double tmpValue;
-        Subtitle tmpSub;
+        BasicSyncSubtitles syncTool = new BasicSyncSubtitles(synchronizedSubtitle, unsynchronizedSubtitle);
+
+        double compareValue = original;
         Subtitle newSub = null;
 
-        for (int i = 0; i < 20; i++) {
-            for (int j = 1; j <= 20; j++) {
-                for (int k = 0; k < 20; k++) {
-                    for (int l = 1; l <= 20; l++) {
-                        syncTool.setSynccedA(engSub.elementAt(i).getStartTime());
-                        syncTool.setSynccedB(engSub.elementAt(engSub.getRowCount() - j).getStartTime());
+        for (int i = 0; i < ITERATIONS; i++) {
+            for (int j = 1; j <= ITERATIONS; j++) {
+                log.info(String.format("Run %d of %d", (i * ITERATIONS + j), ITERATIONS*ITERATIONS));
+                for (int k = 0; k < ITERATIONS; k++) {
+                    for (int l = 1; l <= ITERATIONS; l++) {
+                        syncTool.setSynccedA(synchronizedSubtitle.elementAt(i).getStartTime());
+                        syncTool.setSynccedB(
+                                synchronizedSubtitle.elementAt(synchronizedSubtitle.getRowCount() - j).getStartTime());
 
-                        syncTool.setDessynccedA(porSub.elementAt(k).getStartTime());
-                        syncTool.setDessynccedB(porSub.elementAt(porSub.getRowCount() - l).getStartTime());
+                        syncTool.setDessynccedA(unsynchronizedSubtitle.elementAt(k).getStartTime());
+                        syncTool.setDessynccedB(
+                                unsynchronizedSubtitle.elementAt(unsynchronizedSubtitle.getRowCount() - l)
+                                        .getStartTime());
 
-                        tmpSub = syncTool.synchronize();
-                        tmpValue = SubtitlesUtil.compareSubtitles(engSub, tmpSub);
+                        Subtitle tmpSub = syncTool.synchronize();
+                        double tmpValue = SubtitlesUtil.compareSubtitles(synchronizedSubtitle, tmpSub);
                         if (tmpValue > compareValue) {
-                            if (tmpValue > original) {
-                                log.info("new improovment compared with started value: "
-                                    + String.format("%.2f", tmpValue * 100) + " > "
-                                    + String.format("%.2f", compareValue * 100));
-                                compareValue = tmpValue;
-                                newSub = tmpSub;
-                            }
+                            log.info("new improvement compared with started value: "
+                                     + String.format("%.2f", tmpValue * 100) + " > "
+                                     + String.format("%.2f", compareValue * 100) +
+                                     " with indexes " + String.format("i:%d, j:%d, k:%d, l:%d", i, j, k, l));
+                            compareValue = tmpValue;
+                            newSub = tmpSub;
                         }
-
                     }
                 }
             }
         }
+
         if (newSub != null) {
-            porSubFormat.produce(newSub, new File("output\\legenda.por2.srt"));
+            unsyncSubFormat.produce(newSub, new File("output\\output.srt"));
         }
 
-        log.info("compareTo: " + SubtitlesUtil.compareSubtitles(engSub, porSub));
+        log.info("compareTo: " + SubtitlesUtil.compareSubtitles(synchronizedSubtitle, unsynchronizedSubtitle));
 //        synchronizedSub.split(new SubEntry());
 //        engSub.applyCorrection(1, 1000);
 //        System.out.println(engSub.toString());
-        engSubFormat.produce(engSub, new File("output\\legenda.eng.srt"));
+        syncSubFormat.produce(unsynchronizedSubtitle, new File("output\\legenda.eng.srt"));
         log.info("finished");
     }
 }
